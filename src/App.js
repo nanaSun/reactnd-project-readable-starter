634334424147a46e71c158c798ca71bbe6a83172
cherryvenus <1128yeye@sina.com>
2018-01-02 18:07:59 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className='nav'>
+          <h1 className='header'>readable</h1>
+        </div>
+      </div>
+    );
+  }
+}
+export default App
